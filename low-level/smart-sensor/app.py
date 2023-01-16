4f97c0085d17fe470a20ec7b59bb72efabf11d34
SiropOps <cyril@botalista.community>
2023-01-16 07:17:39 +0100

    feat. (SENSOR) change to Inkbird.IBS-TH1

--- low-level/smart-sensor/app.py
+++ low-level/smart-sensor/app.py
@@ -22,19 +22,14 @@ import pika
 
 sys.path.insert(0, "/usr/local/bin")
 
-#Transmit Handle 0x0021
-TX_CHAR_UUID = btle.UUID('0000fff5-0000-1000-8000-00805F9B34FB')
-#Read Handle 0x0024
-RX_CHAR_UUID = btle.UUID('0000fff3-0000-1000-8000-00805F9B34FB')
+#Read Handle 0x24
+RX_CHAR = 0x24
 
 # Deafults
 LOG_LEVEL = logging.INFO  # Could be e.g. "DEBUG" or "WARNING"
 LOG_FILENAME = '/app/fail/'+os.environ['spring.rabbitmq.queue']+'.log'
 
 FAIL_DIR = "/app/fail/"
-
-PORT = 1         # RFCOMM port
-
 
 class MyLogger(object):
     '''
@@ -80,91 +75,35 @@ sys.stdout = MyLogger(logger, logging.INFO)
 sys.stderr = MyLogger(logger, logging.ERROR)
 
 
+def float_value(nums):
+    # check if temp is negative
+    num = (nums[1]<<8)|nums[0]
+    if nums[1] == 0xff:
+        num = -( (num ^ 0xffff ) + 1)
+    return float(num) / 100
+
 class Data(object):
 
-    def __init__(self, sensor):
-        self.temperature = sensor["temperature"]
-        if self.temperature is not None:
-            self.temperature = self.temperature - 5
-        self.humidity = sensor["humidity"]
+    def __init__(self, readings):
+        self.temperature = float_value(readings[0:2])
+        self.humidity = float_value(readings[2:4])
         self.battery = 0
 
     def __repr__(self):
         return str(self.__dict__)
 
-#Function to return a byte array as zero padded, space separated, hex values
-def convert_to_text(results):
-    hex_results = []
-    for v in range(len(results)):
-        hex_results.append("{:x}".format(results[v]).zfill(2))
-    return " ".join(hex_results)
-
-#Function to send a string to the device as a bytearray and return the results received
-def write_bytes(vals,tx ,rx):
-    write_val = bytearray.fromhex(vals)
-    tx.write(write_val)
-    read_val = rx.read()
-    return read_val
-
-#Function to convert the readings we get back into temperatures and humidities
-def convert_to_readings(response):
-    readings = []
-    for v in range(6):
-        results_position = 6 + (v * 2)
-        reading = int.from_bytes(response[results_position:results_position+2],byteorder='little')
-        reading = reading * 0.0625
-        if reading > 2048:
-            reading = -1 * (4096-reading)
-        readings.append("{:.2f}".format(reading))
-    logger.debug(",".join(readings))
-    for w in range(2, 0, -1):
-        if float(readings[w]) != 0.00:
-           return {"temperature": float(readings[w]), "humidity": float(readings[w+3])}
-    return None
 #Function to recived data
 def getFakeData():
     return {"temperature": -50.0, "humidity": -50.0}
 
-def getDataPoint(available, tx, rx, delta=0):
-    #Data is returned as three pairs of temperature and humidity values
-    data_point = int(available / 3) + delta
-    index = data_point * 3
-    #convert index to hex, padded with leading zeroes
-    index_hex = hex(index)[2:].zfill(4)
-    #reverse the byte order of the hex values
-    index_hex_reversed = index_hex[2:] + index_hex[0:2]
-    #build the request string to be sent to the device
-    hex_string = "07" + index_hex_reversed + "000003"
-    #send the request and get the response
-    response = write_bytes(hex_string, tx, rx)
-    #convert the response to temperature and humidity readings
-    return convert_to_readings(response)
-
 #Function to recived data
 def recv(dev):
     try:
-        #Get handles to the transmit and receieve characteristics
-        tx = dev.getCharacteristics(uuid=TX_CHAR_UUID)[0]
-        rx = dev.getCharacteristics(uuid=RX_CHAR_UUID)[0]
-
-        #Send initial command to get the number of available data points
-        response = write_bytes("0100000000", tx, rx)
-        #The number of available values is stored in the second and third bytes of the response, little endian order
-        available = int.from_bytes(response[1:3], byteorder='little')
-
-        sensor = getDataPoint(available, tx, rx, 1)
-        if sensor is None:
-            logger.debug('sensor is none')
-            sensor = getDataPoint(available,tx, rx)
-        if sensor is None:
-            logger.debug('sensor is none')
-            sensor = getDataPoint(available,tx, rx, -1)
-
+        readings = dev.readCharacteristic(RX_CHAR)
         dev.disconnect()
-        return sensor
-
+        return readings
     except Exception as e:
-        logger.error('General error: ' + str(e))
+        logger.error("Error reading BTLE: {}".format(e))
         return getFakeData()
 
 
@@ -180,7 +119,7 @@ if __name__ == '__main__':
         dev = None
         try:
             try:
-                dev = btle.Peripheral(os.environ['beewi.mac'])
+                dev = btle.Peripheral(os.environ['sensor.mac'], addrType=btle.ADDR_TYPE_PUBLIC)
                 b = recv(dev)
                 logger.info(b)
             except:
@@ -208,7 +147,7 @@ if __name__ == '__main__':
             if is_connected:
                 while True:
                     time.sleep(300)
-                    dev = btle.Peripheral(os.environ['beewi.mac'])
+                    dev = btle.Peripheral(os.environ['sensor.mac'])
                     b = recv(dev)
                     logger.info(b)
                     if b is not None:
